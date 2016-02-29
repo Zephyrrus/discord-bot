2f00ae5de6c11b9bed512aad1ba14440d527314f
Zephy <krisztian1997@gmail.com>
2016-02-29 22:40:51 +0200

    why no  git

--- core/CommandRegister.js
+++ core/CommandRegister.js
@@ -1,4 +1,6 @@
 var logger = require("winston");
+var utils = require("./Utils.js");
+var config = require("../configs/config.json");
 //TODO: separate this into a module loader and command register. Module loader will load an object called "MODULE" which will be a special keyword and can't be used as command name
 
 var example = {
@@ -21,40 +23,66 @@ var example = {
 function CommandRegister() {
   this.commands = {};
   this.cooldowns = [];
+  this.modules = {};
 }
 
 
 CommandRegister.prototype.addModule = function (moduleObject) {
+  var moduleIdentifier;
+  if (!moduleObject["MODULE_HEADER"]){
+    logger.error("[CMD]_This module has no MODULE_HEADER definined, ignoring. (Keys in this module: " +Object.keys(moduleObject) +") ");
+    return;
+  }
   for (var i = 0; i < Object.keys(moduleObject).length; i++) { // TODO: push author, moduleName, description, version, databaseStructure, helpMessage to the object after checking if they're not undefined
+    if(Object.keys(moduleObject)[i] === "MODULE_HEADER"){
+      if(moduleIdentifier) {
+        logger.warn(`[CMD]_Duplicate module header received from '${currentKey.moduleName}', ignoring.`);
+        continue;
+      }
+      var currentKey = moduleObject["MODULE_HEADER"];
+      var abbreviation = utils.abbreviate(currentKey.moduleName, Object.keys(this.modules));
+      this.modules[abbreviation] = currentKey;
+      moduleIdentifier = abbreviation;
+      continue; // this was the special keyword, continue.
+    }
+
     var command = {};
     if (typeof moduleObject[Object.keys(moduleObject)[i]] !== 'object') {
-      logger.error(`[CMD]_Malformed object received. DBG: ` + JSON.stringify(moduleObject, null, "\t"));
+      try{
+        logger.error(`[CMD]_Malformed object received. DBG: ` + JSON.stringify(moduleObject, null, "\t"));
+      } catch(e){
+        logger.error("[CMD]_Malformed object received, JSON parse failed " + e + ". Header: " + moduleObject);
+      }
       continue;
     }
 
     var currentKey = moduleObject[Object.keys(moduleObject)[i]];
     currentKey.name = Object.keys(moduleObject)[i];
     if(this.commands[currentKey.name]){
-      logger.error(`[CMD]_Duplicate command received from '${currentKey.moduleName}'.`);
+      logger.error(`[CMD]_Duplicate command received from '${currentKey.name}'.`);
       return;
     }
     if (!currentKey.permission) {
-      logger.error(`[CMD]_Malformed main command received from '${currentKey.moduleName}'.`);
+      logger.error(`[CMD]_Command has no permissions defined! '${currentKey.name}'.`);
       return;
     }
-    if (!currentKey.description || !currentKey.author || !currentKey.moduleName) {
-      logger.warn(`[CMD]_Module '${currentKey.name}' is missing important identifiers from it's header.`);
+    if (!currentKey.helpMessage || !currentKey.category) {
+      logger.warn(`[CMD]_Command '${currentKey.name}' is missing important identifiers from it's header.`);
     }
-    command.description = currentKey.description || "N/A";
-    command.author = currentKey.author || "N/A";
-    command.version = currentKey.version || "N/A"
+
+    command.description = currentKey.description || "N/A"; // used to describe what this command of the module does
+    command.helpMessage = currentKey.helpMessage || "N/A";
+    command.moduleID = moduleIdentifier;
+    command.cooldown = currentKey.cooldown || config.general.globalcooldown;
+    command.category = currentKey.category || "uncategorized";
+
     if (currentKey.child && !(currentKey.child instanceof Array)) {
-      logger.error(`[CMD]_Malformed command header received from '${currentKey.moduleName}'.`);
+      logger.error(`[CMD]_Malformed command header received from '${currentKey.name}'.`);
       return;
     }
     if (!currentKey.handler) {
       if (!currentKey.child || !(currentKey.child instanceof Array)) {
-        logger.error(`[CMD]_Module '${currentKey.moduleName}' has no handlers declared.`);
+        logger.error(`[CMD]_Module '${currentKey.name}' has no handlers declared.`);
         return;
       }
     }
@@ -67,7 +95,11 @@ CommandRegister.prototype.addModule = function (moduleObject) {
       for (var i = 0; i < currentKey.child.length; i++) {
         if (currentKey.child[i].handler) {
           if (typeof currentKey.child[i].handler === 'function' && currentKey.child[i].name) {
-            command['child'][currentKey.child[i].name] = { handler: currentKey.child[i].handler, permission: currentKey.child[i].permission, helpMessage: currentKey.child[i].helpMessage}
+            if(command['child'][currentKey.child[i].name]){
+              logger.warn(`[CMD]_Child with '${currentKey.name}.${currentKey.child[i].name}' already exists.`)
+              continue;
+            }
+            command['child'][currentKey.child[i].name] = { handler: currentKey.child[i].handler, permission: currentKey.permission + "." + currentKey.child[i].name, helpMessage: currentKey.child[i].helpMessage, cooldown: currentKey.child[i].helpMessage || config.general.globalcooldown}
             logger.info(`[CMD]_Loaded '${currentKey.name}.${currentKey.child[i].name}'.`)
 
           } else {
@@ -77,17 +109,22 @@ CommandRegister.prototype.addModule = function (moduleObject) {
 
       }
     }
+    //TODO: handle aliases
     this.commands[currentKey.name] = command;
+
   }
 }
 
 CommandRegister.prototype.tryExec = function (e, cmd, arguments, callback) {
+  //handle cooldown
+  if(arguments[0]) arguments[0].toLowerCase();
   if (!this.commands[cmd]) {
     return (callback && callback({ errorcode: 404, error: "Command does not exist" }));
   }
-  if (this.commands[cmd].child) {
+  if (this.commands[cmd].child && arguments[0]) {
     //add perm check
-    if (this.commands[cmd]['child'][arguments[0]]) {
+    var lowerSubcmd = arguments[0] ? arguments[0].toLowerCase() : arguments[0];
+    if (this.commands[cmd]['child'][lowerSubcmd]) {
       var subcmd = arguments.shift(); // murder the sub command now
       logger.debug("[CMD]_Found child command for " + cmd + "." + subcmd);
       this.commands[cmd]['child'][subcmd].handler(arguments, e) // call the subcommands
@@ -97,20 +134,24 @@ CommandRegister.prototype.tryExec = function (e, cmd, arguments, callback) {
       if (this.commands[cmd].handler) this.commands[cmd].handler(arguments, e);
     }
   } else {
-    logger.debug("[CMD]_No child commands for " + cmd + ", executing parent command");
+    logger.debug("[CMD]_No child commands for " + cmd + ", executing parent command or no subcmd received.");
     if (this.commands[cmd].handler) this.commands[cmd].handler(arguments, e);
   }
 }
 
-CommandRegister.prototype.getHelpModule = function (cmd) {
-  var help = [];
+CommandRegister.prototype.getHelpCommand = function (cmd) {
+  var help = {};
   if (!this.commands[cmd]) {
-    return (callback && callback({ errorcode: 404, error: "Command does not exist" }));
+    return "Command does not exist";
   }
-  help.push(cmd + " " + (this.commands[cmd].helpMessage || "No help defined."))
+  help.parent = {command: cmd, help: this.commands[cmd].helpMessage}
+  help.category = this.commands[cmd].category;
+  help.moduleID = this.commands[cmd].moduleID;
   if(this.commands[cmd].child){
-    for(var i = 0; i < this.commands[cmd].child.count; i++){
-      help.push(cmd + " " + this.commands[cmd].child[i].name + " " + (this.commands[cmd].child[i].helpMessage || "No help defined."));
+    help.child = [];
+    for(var i = 0; i < Object.keys(this.commands[cmd].child).length; i++){
+      var currentChild = this.commands[cmd].child[Object.keys(this.commands[cmd].child)[i]];
+      help.child.push({command: Object.keys(this.commands[cmd].child)[i], help: currentChild.helpMessage});
     }
   }
   return help;
@@ -128,7 +169,7 @@ CommandRegister.prototype.getPermission = function (cmd, arguments, cb) {
 
 }
 
-CommandRegister.prototype.getAllModules = function () {
+CommandRegister.prototype.getAllCommands = function () {
   return (Object.keys(this.commands));
 }
 
