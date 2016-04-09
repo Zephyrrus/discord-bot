98e4e2d742ba5da439b0afe19563c10acd1b174e
Zephy <krisztian1997@gmail.com>
2016-04-09 10:49:45 +0300

    Minimal youtube api helper

---
+++ Mods/youtube/_youtube.js
@@ -0,0 +1,30 @@
+var http = require('follow-redirects').https;
+if (GLOBAL.MODE === "production") {
+  var auth = require('../../configs/auth.json'); // or remove ./ for absolute path ^_^
+}
+else {
+  var auth = require('../../configs/auth_dev.json'); // or remove ./ for absolute path ^_^
+}
+
+module.exports = {
+  gettitlefromid: function(id, callback){
+    var url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + id + "&key=" + auth.google.apikey;
+    http.get(url, function(res){
+      var body = '';
+
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+      res.on('end', function(){
+          var response = JSON.parse(body);
+          if(response.items[0] != undefined)
+            return callback({"title": response.items[0].snippet.localized.title, "tags": response.items[0].snippet.tags});
+          else
+            return callback(undefined);
+      });
+  }).on('error', function(e){
+        console.log("[Youtube] Got an error: ", e);
+  });
+
+  }
+}
