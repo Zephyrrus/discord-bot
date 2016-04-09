e0e95394c9371de75cfd2cacbe880aca0dac29cd
Zephy <krisztian1997@gmail.com>
2016-04-09 10:48:43 +0300

    Whois fixes

--- Mods/ModDebug.js
+++ Mods/ModDebug.js
@@ -20,7 +20,8 @@ module.exports = {
         moduleName: "Test module",
         version: "1.0.0",
         author: "Zephy",
-        description: ""
+        description: "",
+        //setup: setup
     },
     "debug": {
         handler: tryHelp,
@@ -53,7 +54,7 @@ module.exports = {
         helpMessage: "gets information about a specific user.",
         category: "Info",
         handler: doWhois,
-        params: [{ id: "userID", type: "mention", required: true }]
+        params: [{ id: "userID", type: "mention", required: false }]
     },
     "log": {
         helpMessage: "log",
@@ -78,19 +79,20 @@ function status(e, args) {
     e.respond("- Author: <@" + e.config.general.masterID + "> [Zephy]\n\
 - Version: " + e._disco._version + "\n\
 - Library: Discord.io \n\
+- Source code: https://github.com/Zephyrrus/discord-bot/ \n\
 **Stats for current instance**\n\
 - Uptime: " + utils.tm(e._disco._startTime / 1000) + " [ " + utils.getUptimeString(e._disco._startTime / 1000) + "]\n\
 - Server time: " + now.toString() + "\n\
 - Connected to: " + Object.keys(e._disco.bot.servers).length + " servers, " + channelCount + " channels, " + userCount + " users\n\
 - Modules: " + Object.keys(e._disco.cm.modules).length + "\n\
 - Commands: " + (Object.keys(e._disco.cm.commands).length + commandCount) + "\n\
-- Channel NSFW filter: " + !e.allowNSFW + " (TRUE means no NSFW in this channel/server).")
+- Channel NSFW filter: " + !e.allowNSFW + " (TRUE means no NSFW in this channel/server).");
 }
 
 function getRoles(e, args) {
     res = "";
     for (var role in e._disco.bot.servers[e.serverID].roles) {
-        res += role + ":" + e._disco.bot.servers[e.serverID].roles[role].name + " [position: " + e._disco.bot.servers[e.serverID].roles[role].position + "]\n"
+        res += role + ":" + e.clean(e._disco.bot.servers[e.serverID].roles[role].name) + " [position: " + e._disco.bot.servers[e.serverID].roles[role].position + "]\n";
     }
     e.mention().respondLong("Roles on this server: \n```css\n" + res + "```");
 
@@ -104,13 +106,13 @@ function doHelp(e, args) {
     var help = e._disco.cm.getHelpCommand(args.command);
     if (typeof help === "object") {
         var str = "";
-        str += `**Category**: ${help.category}**\nModuleID**: ${help.moduleID}\n`
-        if (help.parent.command) str += `\n${help.parent.command} ${help.parent.arguments} - **${help.parent.help}**`
+        str += `**Category**: ${help.category}**\nModuleID**: ${help.moduleID}\n`;
+        if (help.parent.command) str += `\n${help.parent.command} ${help.parent.arguments} - **${help.parent.help}**`;
         if (help.child.length > 0) {
             for (var i = 0; i < help.child.length; i++)
-                str += `\n\t${help.parent.command} ${help.child[i].command} ${help.child[i].arguments} - **${help.child[i].help}**`
+                str += `\n\t${help.parent.command} ${help.child[i].command} ${help.child[i].arguments} - **${help.child[i].help}**`;
         }
-        e.pmRespondLong(str);
+        e.pm(str);
         e.mention().respond("Please check your private messages. **EXPLOSION**");
     }
 }
@@ -133,16 +135,16 @@ function getPermissionHelp(e, args) {
     for (var cmd in e._disco.cm.commands) {
         var help = e._disco.cm.getHelpPermission(cmd, e.userID, e.serverID);
         if (typeof help === "object") {
-            if (help.parent.command) str += `\n${help.parent.command} ${help.parent.arguments} - **${help.parent.help}**`
+            if (help.parent.command) str += `\n${help.parent.command} ${help.parent.arguments} - **${help.parent.help}**`;
             if (help.child && help.child.length > 0) {
                 for (var i = 0; i < help.child.length; i++)
-                    str += `\n\t${help.parent.command} ${help.child[i].command} ${help.child[i].arguments} - **${help.child[i].help}**`
+                    str += `\n\t${help.parent.command} ${help.child[i].command} ${help.child[i].arguments} - **${help.child[i].help}**`;
             }
             str += "\n";
         }
         //console.log(cmd);
     }
-    e.pmRespondLong(str);
+    e.pm(str);
     e.mention().respond("Please check your private messages. **EXPLOSION**");
 }
 
@@ -151,31 +153,43 @@ function dumpJson(e, args) {
 }
 
 function doWhois(e, args) {
-    var mentionedUser = e._disco.bot.servers[e.serverID].members[args.userID];
+    args.userID = args.userID || e.userID;
+    var mentionedUser;
+    str = "USER-INFO: \n";
+    if(e._disco.bot.servers[e.serverID]) {
+        mentionedUser = e._disco.bot.servers[e.serverID].members[args.userID];
+    }
+    if(mentionedUser === undefined) {
+            var stradd = "\n\tTHIS USER IS NOT FROM THIS SERVER\n\n";
+            for (var sid in e._disco.bot.servers) {
+                if (e._disco.bot.servers.hasOwnProperty(sid)) {
+                    if(e._disco.bot.servers[sid].members[args.userID]) {
+                        mentionedUser = e._disco.bot.servers[sid].members[args.userID];
+                    }
+                }
+            }
+    }
     if (!mentionedUser) {
         e.mention().respond("I have no information about that user.");
         return;
     }
     str = "USER-INFO: \n";
-    str += `\tName: ${mentionedUser.user.username}\n`;
+    str += stradd || "";
+    str += `\tName: ${e.clean(mentionedUser.user.username)}\n`;
     str += `\tID: ${mentionedUser.user.id}\n`;
     str += `\tDiscriminator: ${mentionedUser.user.discriminator}\n`;
     str += `\tAvatar URL: https://cdn.discordapp.com/avatars/${mentionedUser.user.id}/${mentionedUser.user.avatar}.jpg\n`;
-    str += "\nSERVER-INFO: \n"
+    str += "\nSERVER-INFO: \n";
     str += `\tJoined at: ${new Date(Date.parse(mentionedUser.joined_at))}\n`;
     str += `\tCurrent status: ${mentionedUser.status ? mentionedUser.status: "Offline"}\n`;
-    if (mentionedUser.game != null) str += `\tPlaying: ${mentionedUser.game.name}\n`;
+    if (mentionedUser.game !== null) str += `\tPlaying: ${e.clean(mentionedUser.game.name)}\n`;
     str += `\tRoles: `;
     for (var i = 0; i < mentionedUser.roles.length; i++) {
-        if (e._disco.bot.servers[e.serverID].roles[mentionedUser.roles[i]].name == "@everyone") {
-            str += "[at]everyone";
-        } else {
-            str += e._disco.bot.servers[e.serverID].roles[mentionedUser.roles[i]].name;
-        }
+        str += e.clean(e._disco.bot.servers[e.serverID].roles[mentionedUser.roles[i]].name);
         if (i + 1 != mentionedUser.roles.length) str += ", ";
     }
     str += "\n\n";
-    str += `VOICE-INFO:\n\tMuted:${mentionedUser.mute}\n\tDeafen:${mentionedUser.deaf}`
+    str += `VOICE-INFO:\n\tMuted:${mentionedUser.mute}\n\tDeafen:${mentionedUser.deaf}`;
     e.code(str, "css").respond();
 }
 
@@ -192,21 +206,29 @@ var database = new(require("../core/Database/databaseHandler.js"))('logs', datab
 
 function getMessages(e, args) {
 
-    function _getMessage(channelID, limit, currentSlice, before, maximumSlices) {
+    function _getMessage(channelID, limit, currentSlice, before, maximumSlices, retries) {
         currentSlice = currentSlice || 0;
+        retries = retries || 0;
         var o = { channel: e.channelID, before: before, limit: 100 };
         e._disco.bot.getMessages(o, function (error, messageArr) {
-            if (error && error.statusCode) return console.log(error);
-            if (error && !error.statusCode) _getMessage(channelID, limit, currentSlice, before, maximumSlices);
+          if(error){
+            if (error.statusCode) {return _getMessage(channelID, limit, currentSlice, before, maximumSlices, retries++);}
+            if (!error.statusCode) {return _getMessage(channelID, limit, currentSlice, before, maximumSlices, retries++);}
+            console.log(error);
+          }
 
+            if(retries > 5) return;
+
+            if (!messageArr){ _getMessage(channelID, limit, currentSlice, before, maximumSlices, retries++); }
             for (var i = 0; i < messageArr.length; i++) {
-                database.insert({ channelID: messageArr[i].channel_id, name: messageArr[i].author.username, content: messageArr[i].content, userID: messageArr[i].author.id, timestamp: (new Date(Date.parse(messageArr[i].timestamp)) | 0) }, function (err) {
+                database.insert({ channelID: messageArr[i].channel_id, name: messageArr[i].author.username, content: messageArr[i].content, userID: messageArr[i].author.id, timestamp: Math.floor(new Date(Date.parse(messageArr[i].timestamp))) }, function (err) {
                     if (err) return;
                 });
             }
             if (maximumSlices == currentSlice) return;
             if (messageArr.length > 0) {
-                _getMessage(channelID, limit, currentSlice++, messageArr[messageArr.length - 1].id, maximumSlices);
+                retries = 0;
+                _getMessage(channelID, limit, currentSlice++, messageArr[messageArr.length - 1].id, maximumSlices, retries);
             }
 
         });
