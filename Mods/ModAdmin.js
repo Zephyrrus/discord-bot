db0f4f63f17181ee9077cfbfcb0fe09aef49256b
Zephy <krisztian1997@gmail.com>
2016-04-09 10:49:25 +0300

    Added NSFW toggle for channels

--- Mods/ModAdmin.js
+++ Mods/ModAdmin.js
@@ -10,11 +10,45 @@ module.exports = {
         helpMessage: "Sets the bot's currently playing indicator.",
         category: "Admin",
         handler: setStatus,
+    },
+    "lewd": {
+        helpMessage: "Enabled NSFW in the current channel.",
+        category: "Admin",
+        handler: setLewd,
+        params: [{
+            id: "nsfw",
+            type: "boolean",
+            required: true
+        }]
     }
-}
+};
 
 
 function setStatus(e, args) {
-  e._disco.setStatus(args._str);
-  e.mention().respond("changed status to: *" + args._str + "*")
+    e._disco.setStatus(args._str);
+    e.mention().respond("changed status to: *" + args._str + "*");
+}
+
+function setLewd(e, args) {
+    if (args.nsfw) {
+        if (e.database.nsfwChannels.indexOf(e.channelID) > -1) {
+            e.mention().respond("I am already *lewd*  in this channel \uD83D\uDE33");
+            return;
+        }
+        e.database.nsfwChannels.push(e.channelID);
+        e.database.saveConfig("nsfwChannels");
+        e.mention().respond("From now on I will be lewd in this channel. \uD83D\uDE33");
+        return;
+    }else{
+      if (e.database.nsfwChannels.indexOf(e.channelID) == -1) {
+          e.mention().respond("I was never being lewd in this channel. \uD83D\uDE22");
+          return;
+      }
+      e.database.nsfwChannels.splice(e.database.nsfwChannels.indexOf(e.channelID), 1);
+      e.database.saveConfig("nsfwChannels");
+      e.mention().respond("I will stop being lewd here, I am sorry \uD83D\uDE22");
+      return;
+    }
+
+
 }
